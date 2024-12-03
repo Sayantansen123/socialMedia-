b82c60865d9807ca54a9fab475ff66122087b176
Sayantansen123 <riosayan123@gmail.com>
2024-12-04 00:49:29 +0530

    backend settup started

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { followUnfollowUser, loginUser, logoutUser, signupUser } from "../controllers/userController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+
+const router = express.Router();
+
+router.post("/signup",signupUser)
+router.post("/login",loginUser)
+router.post("/logout",logoutUser)
+router.post("/follow/:id", protectRoute, followUnfollowUser); // Toggle state(follow/unfollow)
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import connectDB from "./db/connectDb.js"
+import userRoutes from "./routes/userRoutes.js"
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());//to parse json data in req.body
+app.use(express.urlencoded({extended: false})); // to parse from data in the req body
+app.use(cookieParser())
+
+
+//routes
+app.use("/api/users",userRoutes)
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(PORT,()=>(console.log(`server started at port http://localhost:${PORT}`)));
+
