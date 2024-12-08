9cef40be19a16d14c3c487b47ebacdebafe1ee31
Sayantansen123 <riosayan123@gmail.com>
2024-12-08 22:46:08 +0530

    delete post route created succesfully

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,9 +1,11 @@
+import Post from "../models/postModel.js";
 import User from "../models/userModel.js";
 
 const createPost = async (req, res) => {
-
+    
     try {
         const { postedBy, text, img } = req.body;
+        console.log(text)
         
         if (!postedBy || !text) {
             return res.status(400).json({ error: "Postedby and text fields are required" });
@@ -32,6 +34,40 @@ const createPost = async (req, res) => {
         console.log(err);
     }
 
-}
+};
 
-export { createPost }
+const getPost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
+const deletePost = async (req,res) =>{
+    try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		if (post.postedBy.toString() !== req.user._id.toString()) {
+			return res.status(401).json({ error: "Unauthorized to delete post" });
+		}
+		await Post.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ message: "Post deleted successfully" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
+
+
+export { createPost,getPost,deletePost}
